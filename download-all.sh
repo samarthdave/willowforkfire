d1d0162815e0a591f7ff8e7ae94877e1eb0330c0
Samarth Dave <samarthdave@users.noreply.github.com>
2017-09-16 19:58:49 -0500

    Decrease overall font size, conf room form edits, rounded items (carousel, blue wells), important links

--- download-all.sh
+++ download-all.sh
@@ -1,15 +1,19 @@
 # dowloading all pages from express server so i can use partials
 # see https://github.com/samarthdave/express-static-gen for more
+URL=localhost:3000
 
-curl localhost:3000/history > history.html
-curl localhost:3000/commissioners > commissioners.html
-curl localhost:3000/command > command.html
-curl localhost:3000/apparatus > apparatus.html
-curl localhost:3000/public-event-signup > public-event-signup.html
-curl localhost:3000/public-appearance > public-appearance.html
-curl localhost:3000/community-training > community-training.html
-curl localhost:3000/conference-room > conference-room.html
-curl localhost:3000/station-tour > station-tour.html
-curl localhost:3000/contacts > contacts.html
-curl localhost:3000/locations > locations.html
-curl localhost:3000/service-area > service-area.html
+curl $URL/main.css > styles/main.css
+
+curl $URL > index.html
+curl $URL/history > history.html
+curl $URL/commissioners > commissioners.html
+curl $URL/command > command.html
+curl $URL/apparatus > apparatus.html
+curl $URL/public-event-signup > public-event-signup.html
+curl $URL/public-appearance > public-appearance.html
+curl $URL/community-training > community-training.html
+curl $URL/conference-room > conference-room.html
+curl $URL/station-tour > station-tour.html
+curl $URL/contacts > contacts.html
+curl $URL/locations > locations.html
+curl $URL/service-area > service-area.html
